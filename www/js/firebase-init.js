8a756f6d308bfedaaaf5ac6c3e400229ba3eb82f
kisoga4-commits <kisoga4@gmail.com>
2026-03-29 12:01:22 +0700

    Reorganize PWA web assets into www directory

---
+++ www/js/firebase-init.js
@@ -0,0 +1,8 @@
+(() => {
+  'use strict';
+  // Placeholder for future Firebase initialization.
+  // Intentionally left minimal for current local/PWA setup.
+  window.FakduFirebase = window.FakduFirebase || {
+    ready: false
+  };
+})();
